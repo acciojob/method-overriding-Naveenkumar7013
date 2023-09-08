75a8d31587ceb318baeec2eafc3f6d7949d455cd
Naveenkumar7013 <telugunaveen55@gmail.com>
2023-09-08 13:19:47 +0530

    2

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -17,7 +17,8 @@ public class Main {
 
     public static void main(String[] args) {
         B obj = new B();
-        System.out.println( obj.meth());
+     // System.out.println( obj.meth());
+        obj.meth();
 
 //        B obj1 = new B();
 //        System.out.println( obj1.meth());
